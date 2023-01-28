ebbe3e6d28a08d7e7d04ec6589d3a8a229a07e38
Kenneth Godwin <abydon@netnomes.com>
2023-01-28 13:17:23 -0500

    Initial commit

---
+++ js/style.js
@@ -0,0 +1,6 @@
+/* feather icons */
+feather.replace()
+
+/* bootstrap tooltips */
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
